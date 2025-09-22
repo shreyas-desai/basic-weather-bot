3c4a479d1fc5a3ae402790fc28f8835a228632ac
Shreyas Desai <shreyasdesai3013@gmail.com>
2025-09-22 10:34:04 -0400

    Sample weather agent

---
+++ multi_tool_agent/agent.py
@@ -0,0 +1,16 @@
+from google.adk.agents import Agent
+
+from multi_tool_agent.tools import get_current_time, get_weather
+
+
+root_agent = Agent(
+    name="weather_time_agent",
+    model="gemini-2.0-flash",
+    description=(
+        "Agent to answer questions about the time and weather in a city."
+    ),
+    instruction=(
+        "You are a helpful agent who can answer user questions about the time and weather in a city."
+    ),
+    tools=[get_weather, get_current_time],
+)
